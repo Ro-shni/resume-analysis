f078565258071179994ba5af170a6fa26b66fff3
Ro-shni <roshninekkanti@gmail.com>
2024-05-18 06:55:07 +0530

    Updated database connection configuration for remote MySQL access

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ import pymysql
 import pafy
 import plotly.express as px
 from Courses import ds_course,web_course,android_course,ios_course,uiux_course, resume_videos, interview_videos
+from sqlalchemy import create_engine,text
 
 # Function to fetch YouTube video title
 def fetch_yt_video(link):
@@ -69,7 +70,7 @@ def course_recommender(course_list):
     return rec_course
 
 # Database connection
-connection = pymysql.connect(host='localhost',user='root', password="@Swaruparam12",database='resume')
+connection = pymysql.connect(host="192.168.137.1",user="root", password="@Swaruparam12",database="resume")
 cursor = connection.cursor()
 
 # Function to insert data into the database
